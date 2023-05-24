dad7d63af828e2857be3728067db61cee49b6abe
Phoebe Swaine <peswaine@gmail.com>
2023-05-24 11:11:51 +0100

    Trying assertEquals to search for added files - running the program might be what isn't working and causing error so have implemented a wrap around to catch any errors given - still broken

--- Bug_Report.md
+++ Bug_Report.md
@@ -1,2 +1,3 @@
 ![Screenshot 2023-05-23 at 15.27.19.png](..%2F..%2FDesktop%2FScreenshot%202023-05-23%20at%2015.27.19.png)
 ![Screenshot 2023-05-23 at 15.29.21.png](..%2F..%2FDesktop%2FScreenshot%202023-05-23%20at%2015.29.21.png)
+
--- Test.py
+++ Test.py
@@ -15,9 +15,11 @@ class DocumentUpdaterTestCase(unittest.TestCase):
         with open(os.path.join(self.target_dir, "allowlist"), "w") as allowlist_file:
             allowlist_file.write("Doe.txt\n")
 
+
     def tearDown(self):
         # Clean up the target directory after each test
         shutil.rmtree(self.target_dir)
+
 
     def test_file_added_to_originals_only(self):
         # Add a file to the originals directory
@@ -36,6 +38,7 @@ class DocumentUpdaterTestCase(unittest.TestCase):
 
         # Assert that the file does not exist in the finals directory
         self.assertFalse(os.path.exists(os.path.join(self.target_dir, "finals", filename)))
+
 
     def test_file_added_to_originals_and_allowlist(self):
         # Add a file to the originals directory
@@ -59,6 +62,7 @@ class DocumentUpdaterTestCase(unittest.TestCase):
         # Assert that the file does not exist in the finals directory
         self.assertFalse(os.path.exists(os.path.join(self.target_dir, "finals", filename)))
 
+
     def test_file_in_originals_allowlist_and_updates(self):
         # Add a file to the originals directory
         filename = "Doe.txt"
@@ -74,16 +78,35 @@ class DocumentUpdaterTestCase(unittest.TestCase):
             file.write("Dr Doe\nMakers Academy\nZetland House\nLondon\nEC2A 4HJ\n")
 
         # Run the program
-        subprocess.check_output(["python3", "document_updater.py", self.target_dir])
+        try:
+            subprocess.check_output(["python3", "document_updater.py", self.target_dir])
+        except subprocess.CalledProcessError as e:
+            print("Error executing document_updater.py:", e.output)
+        # subprocess.check_output(["python3", "document_updater.py", self.target_dir])
 
-        # Assert that the file remains unchanged in the originals directory
-        self.assertTrue(os.path.exists(os.path.join(self.target_dir, "originals", filename)))
+        # # Assert that the file remains unchanged in the originals directory
+        # self.assertTrue(os.path.exists(os.path.join(self.target_dir, "originals", filename)))
 
-        # Assert that the file remains unchanged in the updates directory
-        self.assertTrue(os.path.exists(os.path.join(self.target_dir, "updates", filename)))
+        # # Assert that the file remains unchanged in the updates directory
+        # self.assertTrue(os.path.exists(os.path.join(self.target_dir, "updates", filename)))
 
-        # Assert that the file is copied to the finals directory
-        self.assertTrue(os.path.exists(os.path.join(self.target_dir, "finals", filename)))
+        # # Assert that the file is copied to the finals directory
+        # self.assertTrue(os.path.exists(os.path.join(self.target_dir, "finals", filename)))
+
+        # Assert that the content of the file remains unchanged in the originals directory
+        with open(os.path.join(self.target_dir, "originals", filename), "r") as file:
+            original_content = file.read()
+        with open(os.path.join(self.target_dir, "finals", filename), "r") as file:
+            updated_content = file.read()
+        self.assertEqual(original_content, updated_content)
+
+        # Assert that the content of the file remains unchanged in the updates directory
+        with open(os.path.join(self.target_dir, "updates", filename), "r") as file:
+            original_content = file.read()
+        with open(os.path.join(self.target_dir, "finals", filename), "r") as file:
+            updated_content = file.read()
+        self.assertEqual(original_content, updated_content)
+
 
     def test_file_not_in_originals_but_in_allowlist_and_updates(self):
         # Add a file to the allowlist
@@ -96,16 +119,36 @@ class DocumentUpdaterTestCase(unittest.TestCase):
             file.write("Dr Doe\nMakers Academy\nZetland House\nLondon\nEC2A 4HJ\n")
 
         # Run the program
-        subprocess.check_output(["python3", "document_updater.py", self.target_dir])
+        try:
+            subprocess.check_output(["python3", "document_updater.py", self.target_dir])
+        except subprocess.CalledProcessError as e:
+            print("Error executing document_updater.py:", e.output)
+        # subprocess.check_output(["python3", "document_updater.py", self.target_dir])
+
+        # # Assert that the file is not copied to the originals directory
+        # self.assertFalse(os.path.exists(os.path.join(self.target_dir, "originals", filename)))
+
+        # # Assert that the file remains unchanged in the updates directory
+        # self.assertTrue(os.path.exists(os.path.join(self.target_dir, "updates", filename)))
+
+        # # Assert that the file is copied to the finals directory
+        # self.assertTrue(os.path.exists(os.path.join(self.target_dir, "finals", filename)))
 
         # Assert that the file is not copied to the originals directory
         self.assertFalse(os.path.exists(os.path.join(self.target_dir, "originals", filename)))
 
-        # Assert that the file remains unchanged in the updates directory
-        self.assertTrue(os.path.exists(os.path.join(self.target_dir, "updates", filename)))
+        # Assert that the content of the file remains unchanged in the updates directory
+        with open(os.path.join(self.target_dir, "updates", filename), "r") as file:
+            original_content = file.read()
+        with open(os.path.join(self.target_dir, "finals", filename), "r") as file:
+            updated_content = file.read()
+        self.assertEqual(original_content, updated_content)
 
-        # Assert that the file is copied to the finals directory
-        self.assertTrue(os.path.exists(os.path.join(self.target_dir, "finals", filename)))
+        # Assert that the content of the file is the same in the finals directory
+        with open(os.path.join(self.target_dir, "finals", filename), "r") as file:
+            final_content = file.read()
+        self.assertEqual(updated_content, final_content)
+
 
     def test_file_not_in_any_directories(self):
         # Add a file name to the allowlist
@@ -124,7 +167,6 @@ class DocumentUpdaterTestCase(unittest.TestCase):
 
         # Assert that the file does not exist in the finals directory
         self.assertFalse(os.path.exists(os.path.join(self.target_dir, "finals", filename)))
-
 
 
 if __name__ == "__main__":
